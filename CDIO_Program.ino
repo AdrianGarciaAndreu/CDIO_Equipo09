2a83db5875ec8bd23f0e9633f04e1806058404c0
Adrián García (Theidel) <adrielkaiser@gmail.com>
2018-12-10 01:27:03 +0100

    Add files via upload
    Se han añadido correcciones al sensor GPS.
    Se ha implementado lecturas de un sensor de aceleración con giroscopio
    Se ha añadido lectura de iluminación a través de un fotodiodo.
    Se han unificado dichas funcionalidades en la librería principal.
    
    Se ha deshabilitado temporalmente la lectura barometrica por ausencia de dicho sensor.

--- CDIO_Program.ino
+++ CDIO_Program.ino
@@ -1,13 +1,16 @@
-//#include "Funciones.h"
 #include "Sensors.h"
+#define DATOS_A_ENVIAR 4 //Numero de medidas a enviar al servidor REST (Entre 1 y 8)
+
 
 SalinitySensor ss(5,0); //Sensor de salinidad, pin 5 = salida, pin 0 = entrada
 HumitySensor hr(1); //Sensor de humeddad relativa, pin 1 = entrda analogica
 TemperatureSensor tr(2); //Sensor de temperatura, pin 2 = entrada anlogica
 
-PressureSensor pr; //Sensor de presión, SDA = entrada digital, SCL = señal de reloj.
+//PressureSensor pr; //Sensor de presión, SDA = entrada digital, SCL = señal de reloj.
 
 GPSensor gps;
+
+LuminitySensor Ls(3);
 
 void setup() {
 
@@ -18,28 +21,37 @@ void setup() {
 }
 
 void loop() {
-  //getReadings();
-  //geoLocating();
 
-  /*ss.readSalinity();
-  hr.readHR();
-  pr.readPressure();
-  tr.readAmbientTemp();
+
+    /* Envio de datos a un servidor a través de HTTP
+    String datos[ DATOS_A_ENVIAR + 1]; //Cadena de datos a enviar al servidor    
+    datos[1] = String(hr.readHR()); //Se envia el dato de humedad
+    datos[2] = String(ss.readSalinity()); //Se envía el dato de salinidad
+    datos[3] = String(pr.readPressure()); //Se envía el dato de presión atmosferica
+    datos[4] = String(tr.readAmbientTemp()); //Se envía el dato de temperatura ambiente
+    //data[5] = String(); //Se envía el dato de iluminación
+
+    //HTTPPost( datos, DATOS_A_ENVIAR );
+    HTTPGet( datos, DATOS_A_ENVIAR ); //Se envían los datos vía GET.
+
 */
-  //Prueba de datos
-  Serial.print("HR: "); Serial.print(hr.readHR()); Serial.println("%");
-  Serial.print("Salinity: "); Serial.print(ss.readSalinity()); Serial.println("%");
-  Serial.print("Pressure: "); Serial.print(pr.readPressure()); Serial.println("mb");
-  Serial.print("Temp.: ");  Serial.print(tr.readAmbientTemp()); Serial.println("ºC");
-  
-  Serial.println("");
-  Serial.print("Location: ");  Serial.print(gps.getLocation()); Serial.println("");
-  Serial.println();
-  delay(1500);
 
+ 
+  //Se comprueba si el dispositivo está en movimiento
+  String tmpMovement=readPosition();
+  if(tmpMovement.length()>0){
+    Serial.println(readPosition());
+  }
 
-//getReadings();
-  
-  //NO SE MUESTRAN DATOS POR PANTALLA
+    Serial.print("HR: "); Serial.print(hr.readHR()); Serial.println("%");
+    Serial.print("Salinity: "); Serial.print(ss.readSalinity()); Serial.println("%");
+    Serial.print("Temp.: ");  Serial.print(tr.readAmbientTemp()); Serial.println("ºC");
+   // Serial.print("Pressure: "); Serial.print(pr.readPressure()); Serial.println("mb");
+    Serial.print("Iluminacion: "+Ls.readLuminity());  Serial.println("");
+    Serial.println("");
+
+    Serial.print(gps.getLocation()); Serial.println("");
+    Serial.println("");
+    delay(1500);
   
 }
--- Sensors.h
+++ Sensors.h
@@ -4,17 +4,152 @@
 #include <SFE_BMP180.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+
 
 //Parametros del sensorGPS, se entiende que solo habrá un sensor GPS
-#define RX_PIN 12
-#define TX_PIN 13
-#define INIT_PIN 15
+#define RX_PIN 12 //Pin de lectura (UART virtual)
+#define TX_PIN 13 //Pin de escritura (UART virtual)
+#define INIT_PIN 15 //Pin para enviar señal de inicio/fin
 #define GPS_BAUD 4800  //  velocidad de comunicación serie 
+
+
+//Definición de direcciones para el sensor de aceleración y giroscópio.
+#define    MPU9250_ADDRESS            0x68
+#define    MAG_ADDRESS                0x0C
+ 
+#define    GYRO_FULL_SCALE_250_DPS    0x00  
+#define    GYRO_FULL_SCALE_500_DPS    0x08
+#define    GYRO_FULL_SCALE_1000_DPS   0x10
+#define    GYRO_FULL_SCALE_2000_DPS   0x18
+ 
+#define    ACC_FULL_SCALE_2_G        0x00  
+#define    ACC_FULL_SCALE_4_G        0x08
+#define    ACC_FULL_SCALE_8_G        0x10
+#define    ACC_FULL_SCALE_16_G       0x18
+
+#define interruptPin  0 //pin para manejar las interrupciones
+volatile int intCounter = 0; //contador de interrupciones
+unsigned int myInterrupts = 0;
+
+
+////////////////////////////////
+//////Parametros conexión WiFi
+////////////////////////////////
+#define WiFi_CONNECTION_UPV
+//Conexion
+#ifdef WiFi_CONNECTION_UPV //Conexion UPV
+  const char WiFiSSID[] = "GTI1";
+  const char WiFiPSK[] = "1PV.arduino.Toledo";
+#else //Conexion fuera de la UPV
+  const char WiFiSSID[] = "MySSID";
+  const char WiFiPSK[] = "MyPassWord";
+#endif
+
+//Servidor de envío
+#if defined(WiFi_CONNECTION_UPV) //Conexion UPV
+  const char Server_Host[] = "proxy.upv.es";
+  const int Server_HttpPort = 8080;
+#elif defined(REST_SERVER_THINGSPEAK) //Conexion fuera de la UPV
+  const char Server_Host[] = "api.thingspeak.com";
+  const int Server_HttpPort = 80;
+#else
+  const char Server_Host[] = "dweet.io";
+  const int Server_HttpPort = 80;
+#endif
 
 
 Adafruit_ADS1115 ads1115(0x48); // instanciación de ADS utilizado.
 SFE_BMP180 pr_bmp180; // Instancia del sensor de presion.
-SoftwareSerial sfSerial(RX_PIN, TX_PIN);
+SoftwareSerial sfSerial(RX_PIN, TX_PIN); //Instancia de UART virtual
+WiFiClient client; //Instancia de conexión WiFi
+
+
+#ifdef REST_SERVER_THINGSPEAK 
+  const char Rest_Host[] = "api.thingspeak.com";
+  String MyWriteAPIKey="5ZKIZ5IY6YOY7M0T"; // Escribe la clave de tu canal ThingSpeak
+#else 
+  const char Rest_Host[] = "dweet.io";
+  String MyWriteAPIKey="PruebaGTI"; // Escribe la clave de tu canal Dweet
+#endif
+
+
+
+/*
+ * Función alternativa de lectura
+ */
+void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
+{
+   Wire.beginTransmission(Address);
+   Wire.write(Register);
+   Wire.endTransmission();
+ 
+   Wire.requestFrom(Address, Nbytes);
+   uint8_t index = 0;
+   while (Wire.available())
+   {
+      Data[index++] = Wire.read();
+   }
+}
+ 
+
+/* 
+ * Funcion alternativa de escritura 
+ */
+void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
+{
+   Wire.beginTransmission(Address);
+   Wire.write(Register);
+   Wire.write(Data);
+   Wire.endTransmission();
+}
+
+
+
+/*
+ * Función para manejar interrupción producida por el sensor de aceleración. 
+ * Está lleva una cuenta de interrupciones y envía/muestra una señal para indicar
+ * el movimiento del dispositivo.
+ */
+ void handleInterrupt()
+{
+  uint8_t intStatus[1];
+  I2Cread(MPU9250_ADDRESS,0x3A, 1, intStatus); //leemos de intStatus para desactivar la interrupcion
+   intCounter++;
+  //digitalWrite(interruptPin,HIGH);
+} 
+
+
+
+
+
+/*
+ * Función para conectar WiFi
+ */
+void connectWiFi()
+{
+
+  #ifdef PRINT_DEBUG_MESSAGES
+    Serial.print("MAC: ");
+    Serial.println(WiFi.macAddress());
+  #endif
+  
+  WiFi.begin(WiFiSSID, WiFiPSK);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    #ifdef PRINT_DEBUG_MESSAGES
+       Serial.println(".");
+    #endif
+    delay(500);
+  }
+  #ifdef PRINT_DEBUG_MESSAGES
+     Serial.println( "WiFi Connected" );
+     Serial.println(WiFi.localIP()); // Print the IP address
+  #endif
+}
+
+
 
 
 /*
@@ -31,16 +166,34 @@ void Initialization(){
   ads1115.setGain(GAIN_ONE); // Rango del ADC: +/- 4.096V (1 bit=2mV)
 
   //Se inicia la comunicación con el sensor de presión BMP 180
-   if(pr_bmp180.begin())
-    Serial.println("BMP180 iniciado correctamente");
-   else{
-    Serial.println("Error al iniciar el BMP180");
-  }
-
+   if(pr_bmp180.begin()){}
+   else{Serial.println("Error al iniciar el BMP180");}
 
   //GPS
   sfSerial.begin(GPS_BAUD); // Inicializar la comunicación con el GPS
   pinMode(INIT_PIN,OUTPUT); 
+
+  //Iniciar comunicación con el acelerometro
+  Wire.begin();
+  I2CwriteByte(MPU9250_ADDRESS, 28, ACC_FULL_SCALE_2_G);
+  pinMode(interruptPin,INPUT_PULLUP); //Configuración del pin que este usara para manejar interrupciones
+  attachInterrupt(digitalPinToInterrupt(interruptPin),handleInterrupt, FALLING); //Condicionamiento de función que manejará las interrupciones
+  I2CwriteByte(MPU9250_ADDRESS, 0x6B, 0);
+  I2CwriteByte(MPU9250_ADDRESS, 0x6C, 7);
+  I2CwriteByte(MPU9250_ADDRESS, 0x1D, 9);
+  
+  I2CwriteByte(MPU9250_ADDRESS, 0x38, 64);
+  I2CwriteByte(MPU9250_ADDRESS, 0x69, 192);
+  I2CwriteByte(MPU9250_ADDRESS, 0x1F, 15);
+  I2CwriteByte(MPU9250_ADDRESS, 0x1E, 2);
+  I2CwriteByte(MPU9250_ADDRESS, 0x6B, 32);
+  I2CwriteByte(MPU9250_ADDRESS, 0x37, 224);
+  Serial.println("Acelerometro OK");
+
+  interrupts(); //Se habilitan las interrupciones
+
+
+ //connectWiFi(); //Inicia conexión WiFi.
 
   
 }
@@ -52,6 +205,95 @@ double mapDecimal(double x, double in_min, double in_max, double out_min, double
  return temp;
 }
 
+
+
+/*
+ * Función para enviar datos vía HTTP POST al servidor. 
+ */
+void HTTPPost(String fieldData[], int numFields){
+    
+    if (client.connect( Server_Host , Server_HttpPort )){
+        
+        //Se construye la cadena de caracteres a enviar como parámetros.   
+        
+        String PostData= "api_key=" + MyWriteAPIKey ;
+        for ( int field = 1; field < (numFields + 1); field++ ){
+            PostData += "&field" + String( field ) + "=" + fieldData[ field ];
+        }     
+        
+        // POST data via HTTP
+        client.println( "POST http://" + String(Rest_Host) + "/update HTTP/1.1" );
+        client.println( "Host: " + String(Rest_Host) );
+        client.println( "Connection: close" );
+        client.println( "Content-Type: application/x-www-form-urlencoded" );
+        client.println( "Content-Length: " + String( PostData.length() ) );
+        client.println();
+        client.println( PostData );
+      
+            Serial.println( PostData ); //Muestra por pantalla los datos enviados al servidor //SOLO PARA DEPURAR
+           
+            //En caso de que el servidor responda, dicha respuesta se envia por pantalla
+            #ifdef PRINT_HTTP_RESPONSE
+              delay(500);
+              Serial.println();
+              while(client.available()){String line = client.readStringUntil('\r');Serial.print(line); }
+              Serial.println();
+              Serial.println();
+            #endif
+    }
+}
+
+
+/*
+ * Función para enviar datos vía HTTP GET al servidor. 
+ */
+void HTTPGet(String fieldData[], int numFields){
+    
+    if (client.connect( Server_Host , Server_HttpPort )){
+           #ifdef REST_SERVER_THINGSPEAK 
+              String PostData= "GET https://api.thingspeak.com/update?api_key=";
+              PostData= PostData + MyWriteAPIKey ;
+           #else 
+              String PostData= "GET http://dweet.io/dweet/for/";
+              PostData= PostData + MyWriteAPIKey +"?" ;
+           #endif
+
+
+           //Construye la cadena de envío
+           
+           for ( int field = 1; field < (numFields + 1); field++ ){
+              PostData += "&field" + String( field ) + "=" + fieldData[ field ];
+           }
+          
+           //Envía datos al servidor
+           client.print(PostData);         
+           client.println(" HTTP/1.1");
+           client.println("Host: " + String(Rest_Host)); 
+           client.println("Connection: close");
+           client.println();
+           
+              Serial.println( PostData ); //Escribe cadena enviada al servidor vía POST //SOLO PARA DEPURAR
+              Serial.println();
+           
+              //Sí hay respuesta del servidor, se escribe por el monitor serie
+              #ifdef PRINT_HTTP_RESPONSE
+                delay(500);
+                Serial.println();
+                while(client.available()){String line = client.readStringUntil('\r');Serial.print(line); }
+                Serial.println();
+                Serial.println();
+              #endif
+           
+    }
+}
+
+
+
+
+
+////////////////////////////////////
+////// Sensores 
+///////////////////////////////////
 
 /*
  * Clase para definir sensores de salinidad
@@ -335,16 +577,146 @@ String GPSensor::getLocation(){
     sprintf(gpsDate,"%d/%d/%d", gps.date.month(),gps.date.day(),gps.date.year()); // Construimos string de datos fecha
     sprintf(gpsTime,"%d/%d/0%d", gps.time.hour(),gps.time.minute(),gps.time.second());  // Construimos string de datos hora
 
-    resultGPS = gpsDate;
+    Serial.print(gps.location.lat(),6);
+    Serial.print('\t');
+    Serial.print(gps.location.lng(),6);
+    Serial.print('\t');
+
+
+    resultGPS = "Location: ";
+    resultGPS = resultGPS + gpsDate;
     resultGPS = resultGPS + ", ";
-    resultGPS = resultGPS + gpsTime;
+    resultGPS = resultGPS + gpsTime + ", ";
+    resultGPS = resultGPS + gps.location.lat() + " lat, " +gps.location.lng()+ " lang";
+    
   }
   else  // Si no recibe los mensajes
   {
-    resultGPS = ("Satellites in view: ");
-    resultGPS = (gps.satellites.value());
+    resultGPS = ("Satelites GPS captados: ");
+    resultGPS = resultGPS + (gps.satellites.value());
   }
   gpsDelay(1500);
 
   return resultGPS;
 }
+
+
+/*
+ * Clase para definir sensores de iluminación
+ */
+class LuminitySensor{
+  private:
+   int input_pin; // pin de recepción de datos analógicos (entrada)
+   int sensorMin, sensorMax; //margenes minimo/maximo del sensor.
+   //int addr; //dirección del ADS a utilizar
+  public:
+    //Constructor, los parametros a introducir son el pin de entrada analogico.
+    LuminitySensor(int);
+    void setMinMax(int,int); //establece minimo/maximo a contemplar
+    String readLuminity(); //Obtiene una medida de la iluminación relativa
+}; 
+
+
+/*
+ * constructor del sensor, establece el pin de entrada, y unos valores de rango por defecto
+ * en función de la calibración al sensor utilizado (%NOMBRE/MODELO_SENSOR%)
+ */
+LuminitySensor::LuminitySensor(int pin_in){
+  this->input_pin=pin_in;
+  this->sensorMin = 100; //Valor por defecto del sensor en oscuridad (tapado)
+  this->sensorMax = 30000; //Valor por defecto del sensor a plena luz de un día soleado
+}
+
+/*
+ * Función para re-calibrar el rango de valores del sensor de humedad;
+ */
+void LuminitySensor::setMinMax(int minimum, int maximum){
+  this->sensorMin=minimum;
+  this->sensorMax=maximum;
+}
+
+/*
+ * Función para tomar medida con el sensor, se devuelve como un número entero.
+ */
+String LuminitySensor::readLuminity(){
+  int readingLu, resultLu;
+  String resultLuToSend;
+  
+  int const LU_MIN = 0;
+  int const LU_MAX = 100;
+  
+  readingLu = ads1115.readADC_SingleEnded(this->input_pin);
+  resultLu = map(readingLu,this->sensorMin, this->sensorMax, 0, 100);
+
+  if (resultLu<LU_MIN ){
+      resultLu=LU_MIN ;
+      }
+  if (resultLu>LU_MAX){
+      resultLu=LU_MAX;
+      }
+
+  if (resultLu<20){
+    resultLuToSend = "Poca luz";
+  }
+  else if(resultLu<80){
+    resultLuToSend = "Luz indirecta o escasa";
+  }
+  else {
+    resultLuToSend = "Luz directa";
+  }
+
+  return resultLuToSend;
+}
+
+
+
+
+
+/*
+ * Funció para leer y mostrar datos de posicionamiento locales, acelerometro/giroscopio
+ */
+String readPosition(){
+    
+    String result;
+    
+    //Serial.print("Numero de interrupciones:");
+    //Serial.println(intCounter, DEC);
+    //delay(500);
+    //Serial.println("Leemos del acelerometro");
+    
+   // ---  Lectura acelerometro y giroscopio --- 
+   uint8_t Buf[14];
+   int FS_ACC=2;
+   //int FS_GYRO=2000;
+   I2Cread(MPU9250_ADDRESS, 0x3B, 14, Buf);
+ 
+   // Convertir registros acelerometro
+   float ax = (Buf[0] << 8 | Buf[1]);
+   float ay = (Buf[2] << 8 | Buf[3]);
+   float az = Buf[4] << 8 | Buf[5];
+ 
+   ax=ax*FS_ACC/32768;
+   ay=ay*FS_ACC/32768;
+   az=az*FS_ACC/32768;
+
+ 
+ 
+   // --- Mostrar valores ---
+ 
+   // Acelerometro
+   /*Serial.println("Lectura Acelerometro");
+   Serial.print("AX="); Serial.print(ax, 2); Serial.print("g"); Serial.print("\t");
+   Serial.print("AY="); Serial.print(ay, 2); Serial.print("g"); Serial.print("\t");
+   Serial.print("AZ="); Serial.print(az, 2); Serial.println("g");
+   Serial.println("");
+  */
+  
+  //movimientos considerados intencionados (desplazamiento forzoso del dispositivo)
+  if(ax>3 || ay>3 || az>1.5){
+    result = "DISPOSITIVO EN MOVIMIENTO ";
+    myInterrupts++;
+    //delay(500);
+    }
+   
+  return result;
+  }
